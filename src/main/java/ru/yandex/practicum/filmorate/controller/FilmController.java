0e318ea4bb3c9c9eb6a00587664ca009ce490b72
Nikolay Abakumov <abackumov.nikolai@yandex.ru>
2024-12-17 18:19:21 +0300

    Первое ревью ТЗ-10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,69 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Collection<Film> getFilms() {
+        log.info("Вызван метод передачи списка всех фильмов");
+        return films.values();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film createFilm(@Valid @RequestBody Film film) {
+        log.info("Начался процесс создания фильма {}", film);
+        validateDataRelease(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Процесс создания фильма {} - успешно завершен", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        log.info("Начался процесс обновления фильма {}", film);
+        validateDataRelease(film);
+        if (!films.containsKey(film.getId())) {
+            log.error("Фильм {} не найден", film);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Фильм не найден");
+        }
+        films.put(film.getId(), film);
+        log.info("Процесс обновления фильма {} - успешно завершен", film);
+        return film;
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    private void validateDataRelease(Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Фильм {} не создан, по причине неверной даты релиза", film);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Дата релиза — не раньше 28 декабря 1895 года");
+        }
+    }
 }
